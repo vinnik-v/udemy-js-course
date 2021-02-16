6e27a42c8a2ab432413d4ae1c531d09d4d86fd59
Vitaliy Vinnik <vinnikviv@gmail.com>
2021-02-16 22:46:25 +0300

    hw stage 2 is finished

--- step-1/js/index.js
+++ step-1/js/index.js
@@ -1,4 +1,4 @@
- "use strict";
+"use strict";
 
 const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', 0);
 
@@ -8,16 +8,30 @@ const personalMovieDB = {
     actors: {},
     genres: [],
     privat: false
+};
+
+let questions = ['Один из последних просмотренных фильмов?', 'На сколько оцените его?'];
+ 
+for (let i = 0; i < 1; i++){
+    let qw1;
+
+    while(!qw1 || qw1.length > 10){
+        qw1 = prompt(questions[0]);
+    }
+
+    let qw2 = prompt(questions[1]);
+
+    personalMovieDB.movies[qw1] = qw2;
+    
 }
 
-const a = prompt('Один из последних просмотренных фильмов?');
-const b = prompt('На сколько оцените его?');
-const c = prompt('Один из последних просмотренных фильмов?');
-const d = prompt('На сколько оцените его?');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
-
-// alert(personalMovieDB)
-
-console.log(personalMovieDB)
+if (personalMovieDB.count < 10){
+    console.log("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30){
+    console.log("Вы классический зритель");
+} else if (personalMovieDB.count > 30){
+    console.log("Вы киноман");
+} else {
+    console.log("Произошла ошибка");
+}
